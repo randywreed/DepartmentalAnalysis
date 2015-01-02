4178c2b0fdc3642b75128c072d8467190d559e1a
Randy Reed <randywreed@gmail.com>
2015-01-02 10:00:37 -0500

    dplyr modify dataset

--- DepartmentAnalysis.r
+++ DepartmentAnalysis.r
@@ -1,4 +1,18 @@
 install.packages("dplyr")
-library(dplyr)
+install.packages("tidyr")
+install.packages("ggvis")
+library(ggvis)
+library(dplyr)1834
+library(tidyr)
 #read in csv
-classbyprof<- read.csv("classbyprof.csv")
+classbyprof<- tbl_df(data = read.csv("classbyprof.csv"))
+relclasses<- classbyprof %>% filter(SUB=="REL") %>%
+  separate(TERM, into=c("Semester","Year"),"\\ ") %>%
+  arrange(COURSE, Year, Semester) 
+  
+relclassdata <- relclasses %>% 
+  select (PROF.:Year, TITLE_SHORT_DESC, ACTUAL, MAX) %>%
+  group_by(COURSE)%>%
+  filter (n()>4)
+
+relclassdata %>% ggvis(~factor(COURSE), ~ACTUAL)
