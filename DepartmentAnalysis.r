bf7b4d5a453f4c154ea85eacef74a68285a9d6c8
Randy Reed <randywreed@gmail.com>
2015-01-05 10:28:26 -0500

    Add summary by course

--- DepartmentAnalysis.r
+++ DepartmentAnalysis.r
@@ -1,18 +1,31 @@
 install.packages("dplyr")
 install.packages("tidyr")
 install.packages("ggvis")
+install.packages("forecast")
+library(forecast)
 library(ggvis)
-library(dplyr)1834
+library(dplyr)
 library(tidyr)
 #read in csv
 classbyprof<- tbl_df(data = read.csv("classbyprof.csv"))
 relclasses<- classbyprof %>% filter(SUB=="REL") %>%
   separate(TERM, into=c("Semester","Year"),"\\ ") %>%
   arrange(COURSE, Year, Semester) 
-  
+
+# group by course
 relclassdata <- relclasses %>% 
   select (PROF.:Year, TITLE_SHORT_DESC, ACTUAL, MAX) %>%
   group_by(COURSE)%>%
   filter (n()>4)
 
+#plot course and total students
+#future: modify so that can adjust the actual number dynamically
 relclassdata %>% ggvis(~factor(COURSE), ~ACTUAL)
+
+#total classes by year by semester
+sumclassdata <- relclasses %>% 
+  select(COURSE, Year, Semester, TITLE_SHORT_DESC, ACTUAL) %>%
+  group_by(Year, Semester, COURSE) %>%
+  summarize(sum(ACTUAL)) %>%
+  arrange(COURSE)
+
